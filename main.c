d47a1d9ee688f8920e0373b103cfb1f58e036bfa
hyorimlee <hl1046@pusan.ac.kr>
2020-03-30 17:55:16 +0900

    add print function

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+int print(int a) {
+	int i;
+	for(i=0;i<a;i++){
+		printf("%i\t");
+		}
+	printf("\n");
+	return 0;
+}
 int main(){
 	printf("Hello my github!\n");
 	return 0;
